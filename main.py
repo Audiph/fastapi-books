b355a815f00fb783e440f07eeb0ad6e94aa3a022
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-10-26 00:52:09 +0800

    Query Parameters

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ BOOKS = [
   {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
   {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
   {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
-  {'title': 'Title Six', 'author': 'Author One', 'category': 'math'}
+  {'title': 'Title Six', 'author': 'Author Six', 'category': 'math'}
 ]
 
 
@@ -24,3 +24,23 @@ async def get_book(book_title: str) -> dict:
       return book
 
   return {'message': 'Book not found'}
+
+
+@app.get('/books/')
+async def get_books_by_category(category: str) -> list:
+  books = []
+  for book in BOOKS:
+    if book['category'].casefold() == category.casefold():
+      books.append(book)
+
+  return books
+
+
+@app.get('/books/{book_author}/')
+async def get_books_by_author_and_category(book_author: str, category: str) -> list:
+  books = []
+  for book in BOOKS:
+    if book['author'].casefold() == book_author.casefold() and book['category'].casefold() == category.casefold():
+      books.append(book)
+
+  return books
