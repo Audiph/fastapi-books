3ec67884737e6895c768a2f2ad1ba62b2a2abf3c
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-10-26 00:08:16 +0800

    add get book by title and add explicit type for params and return type

--- main.py
+++ main.py
@@ -13,5 +13,14 @@ BOOKS = [
 
 
 @app.get('/books')
-async def get_all_books():
+async def get_all_books() -> list:
   return BOOKS
+
+
+@app.get('/books/{book_title}')
+async def get_book(book_title: str) -> dict:
+  for book in BOOKS:
+    if book['title'].casefold() == book_title.casefold():
+      return book
+
+  return {'message': 'Book not found'}
